92212d0735f215af041184e623f464624f552cb5
TOSHIBA <TOSHIBA@ANGEL>
2018-01-12 02:11:39 -0500

    Roles Angel

--- Grupo5/src/grupo5/rnegocio/entidades/Roles.java
+++ Grupo5/src/grupo5/rnegocio/entidades/Roles.java
@@ -1,16 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package grupo5.rnegocio.entidades;
-
 import java.util.Date;
 
-/**
- *
- * @author Usuario
- */
 public class Roles {
     private int id;
     private String nombre;
@@ -57,7 +47,5 @@ public class Roles {
 
     public void setActualizado(Date actualizado) {
         this.actualizado = actualizado;
-    }
-    
-    
+    }   
 }
