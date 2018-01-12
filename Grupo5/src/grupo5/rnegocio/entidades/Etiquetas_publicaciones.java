f206bfa4c0ac20d168fde87bb459a8fafce05722
Wen <Wen@DESKTOP-SPILHLE>
2018-01-12 00:44:52 -0500

    Etiqueta_publicacion Wendy

--- Grupo5/src/grupo5/rnegocio/entidades/Etiquetas_publicaciones.java
+++ Grupo5/src/grupo5/rnegocio/entidades/Etiquetas_publicaciones.java
@@ -1,14 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package grupo5.rnegocio.entidades;
 
-/**
- *
- * @author Usuario
- */
+package grupo5.rnegocio.entidades;
+import java.util.*;
 public class Etiquetas_publicaciones {
-    
+    private int etiqueta_id;
+    private int publicacion_id;
+    private Date creado;
+    private Date actualizado;
+
+    public Etiquetas_publicaciones(int etiqueta_id, int publicacion_id, Date creado, Date actualizado) {
+        this.etiqueta_id = etiqueta_id;
+        this.publicacion_id = publicacion_id;
+        this.creado = creado;
+        this.actualizado = actualizado;
+    }
+
+    public Etiquetas_publicaciones() {
+    }
+
+    public int getEtiqueta_id() {
+        return etiqueta_id;
+    }
+
+    public void setEtiqueta_id(int etiqueta_id) {
+        this.etiqueta_id = etiqueta_id;
+    }
+
+    public int getPublicacion_id() {
+        return publicacion_id;
+    }
+
+    public void setPublicacion_id(int publicacion_id) {
+        this.publicacion_id = publicacion_id;
+    }
+
+    public Date getCreado() {
+        return creado;
+    }
+
+    public void setCreado(Date creado) {
+        this.creado = creado;
+    }
+
+    public Date getActualizado() {
+        return actualizado;
+    }
+
+    public void setActualizado(Date actualizado) {
+        this.actualizado = actualizado;
+    }
 }
