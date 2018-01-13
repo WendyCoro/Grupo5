25ce34187e615d2728fe36d3afebfec53cc7758e
DiegoPatricio <DiegoPatricio@VAIO>
2018-01-13 17:05:21 -0500

    Diego

--- Grupo5/src/grupo5/rnegocio/impl/ImplRoles.java
+++ Grupo5/src/grupo5/rnegocio/impl/ImplRoles.java
@@ -1,14 +1,66 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package grupo5.rnegocio.impl;
 
-/**
- *
- * @author Wen
- */
-public class ImplRoles {
+import grupo5.rnegocio.dao.*;
+import grupo5.rnegocio.entidades.*;
+import gupo5.accesodatos.*;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImplRoles implements IRoles{
+     
+    @Override
+    public int insertar(Roles roles) throws Exception{
+        int numFilasAfectadas=0;
+        String sql="insert into Roles values(?,?,?,?)";
+        List<Parametro> lstPar = new ArrayList<>();
+        lstPar.add(new Parametro(1, roles.getId()));
+        lstPar.add(new Parametro(2, roles.getNombre()));
+        lstPar.add(new Parametro(3, roles.getCreado()));
+        lstPar.add(new Parametro(4, roles.getActualizado()));
+        Conexion con = null;
+        try {
+            con = new Conexion();
+            con.conectar();
+            numFilasAfectadas = con.ejecutarComando(sql,lstPar);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            con.desconectar();
+        }
+        return numFilasAfectadas;
+    }
+
     
+    @Override
+    public List<Roles> obtener() throws Exception{
+    List<Roles> lista = new ArrayList<>();
+        String sql="SELECT * FROM Roles;";
+        Conexion con = null;
+        try{
+            Roles roles = null;
+            con = new Conexion();
+            con.conectar();
+            ResultSet rst = con.ejecutarQuery(sql, null);
+            while (rst.next()){
+                roles = new Roles();
+                roles.setId(rst.getInt(1));
+                roles.setNombre(rst.getString(2));
+                roles.setCreado(rst.getDate(3));
+                roles.setActualizado(rst.getDate(4));
+                lista.add(roles);
+           }
+        }catch (Exception e) {
+            throw e;
+        }finally {
+            if(con!=null)
+            con.desconectar();
+        }
+        return lista;
+    }  
+    
+   @Override
+    public Roles obtener(int codigo) throws Exception{
+        return null;     
+    }  
 }
--- Grupo5/src/grupo5/rnegocio/impl/ImplUsuarios.java
+++ Grupo5/src/grupo5/rnegocio/impl/ImplUsuarios.java
@@ -1,14 +1,76 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package grupo5.rnegocio.impl;
 
-/**
- *
- * @author Wen
- */
-public class ImplUsuarios {
+import grupo5.rnegocio.dao.*;
+import grupo5.rnegocio.entidades.*;
+import gupo5.accesodatos.*;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImplUsuarios implements IUsuario{
+    
+    @Override
+    public int insertar(Usuario usuario) throws Exception{
+        int numFilasAfectadas=0;
+        String sql="insert into Usuario values(?,?,?,?,?,?,?)";
+        List<Parametro> lstPar = new ArrayList<>();
+        lstPar.add(new Parametro(1, usuario.getId()));
+        lstPar.add(new Parametro(2, usuario.getNombre()));
+        lstPar.add(new Parametro(3, usuario.getEmail()));
+        lstPar.add(new Parametro(4, usuario.getPassword()));
+        lstPar.add(new Parametro(5, usuario.getRoles().getId()));
+        lstPar.add(new Parametro(6, usuario.getCreado()));
+        lstPar.add(new Parametro(7, usuario.getActualizado()));
+        Conexion con = null;
+        try {
+            con = new Conexion();
+            con.conectar();
+            numFilasAfectadas = con.ejecutarComando(sql,lstPar);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            con.desconectar();
+        }
+        return numFilasAfectadas;
+    }
+
+    
+    @Override
+    public List<Usuario> obtener() throws Exception{
+    List<Usuario> lista = new ArrayList<>();
+        String sql="SELECT * FROM Usuario;";
+        Conexion con = null;
+        try{
+            Usuario usuario = null;
+            con = new Conexion();
+            con.conectar();
+            ResultSet rst = con.ejecutarQuery(sql, null);
+            while (rst.next()){
+                usuario = new Usuario();
+                usuario.setId(rst.getInt(1));
+                usuario.setNombre(rst.getString(2));
+                usuario.setEmail(rst.getString(3));
+                usuario.setPassword(rst.getString(4));
+                usuario.setCreado(rst.getDate(6));
+                usuario.setActualizado(rst.getDate(6));
+                IRoles rolesdao = new ImplRoles();
+                Roles roles = rolesdao.obtener(rst.getInt(5));
+                usuario.setRoles(roles);
+                lista.add(usuario);
+           }
+        }catch (Exception e) {
+            throw e;
+        }finally {
+            if(con!=null)
+            con.desconectar();
+        }
+        return lista;
+    }  
+     @Override
+    public Usuario obtener(int codigo) throws Exception{
+        return null;
+        
+    }  
     
 }
+
